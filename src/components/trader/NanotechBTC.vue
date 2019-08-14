c648d7e9471bdea75f927fd77e4805c9572ed9c1
paulo <robertolamim@gmail.com>
2019-08-14 16:52:29 -0300

    Fix console erro de render nas nanotechs

--- src/components/trader/NanotechBTC.vue
+++ src/components/trader/NanotechBTC.vue
@@ -388,9 +388,11 @@
                 isModalVisible: false,
                 isInvestWindowVisible: false,
                 isWithdrawalWindowVisible: false,
+                isPendingOperations: false,
                 isPendingOperationsVisible: false,
                 tabclass_invest: 'nav-link active',
                 tabclass_withdrawal: 'nav-link',
+                lists: [''],
                 investment_in: {
                     amount: 0,
                     coin: 1,
