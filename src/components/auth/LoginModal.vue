9b95a91dd28b457d10d7027caf87c38b3f6573c9
Cristiano <cristianovelkan@gmail.com>
2020-02-28 14:23:00 -0300

    mensagem remover 2fa

--- src/components/auth/LoginModal.vue
+++ src/components/auth/LoginModal.vue
@@ -1,64 +1,57 @@
 <template>
-    <modal>
-        <template slot="header">
-            <h6 class="pull-left ml-3 mt-2">
-                Atenção:
-            </h6>
+  <modal>
+    <template slot="header">
+      <h6 class="pull-left ml-3 mt-2">
+        Atenção:
+      </h6>
 
-            <button class="btn btn-grey pull-right"
-                    type="button"
-                    @click="closeModal">
-                <i class="os-icon os-icon-close"></i>
-            </button>
-        </template>
+      <button class="btn btn-grey pull-right"
+              type="button"
+              @click="closeModal">
+        <i class="os-icon os-icon-close"></i>
+      </button>
+    </template>
 
-        <template slot="body">
-            <p>
-                Caros usuários,
+    <template slot="body">
+      <p>
+        Caros usuários,
 
-            <p>Todos os Masternodes que foram desfeitos, e estornadas as moedas para sua carteira, abriremos uma nova
-                janela de recontratação de Masternodes. Para que isso não afete os casos de PIN FOUNDER.</p>
-            <p>
-                Não é necessário abrir ticket para questionar esse tema, assim que abrirmos essa janela de
-                recontratação, deixaremos um recado e avisaremos nos canais oficiais da Liquidex.
-            </p>
-            <p>
-                Essa solução poderá ser esperada para as próximas 2 semanas, dia 01/03 colocaremos no ar, mas somente
-                para os que tiveram os Masternodes desfeitos, e haverá um prazo de 2 semanas para recontratar.
-            </p>
-            <p>
-                Após essa data, não haverá mais contratação de Masternodes via Liquidex, somente a manutenção dos
-                ativos.
-            </p>
+      </p>
 
-            <p>
-                Agradecemos a compreensão. <br>
-                Equipe Liquidex
-            </p>
-        </template>
-        <template slot="footer">
-            <div class="col-sm-12 text-center">
-                <button class="btn btn-primary" type="button" @click="closeModal"> Fechar</button>
-            </div>
-        </template>
-    </modal>
+      <p>
+        Para desativar 2FA, é necessário enviar um e-mail para contato@liquidex.com.br e anexar uma foto tipo selfie do
+        usuário, com o documento com foto e com CPF legível, um papel com data do dia e a frase "QUERO REMOVER O 2FA".
+
+      </p>
+
+      <p>
+        Agradecemos a compreensão. <br>
+        Equipe Liquidex
+      </p>
+    </template>
+    <template slot="footer">
+      <div class="col-sm-12 text-center">
+        <button class="btn btn-primary" type="button" @click="closeModal"> Fechar</button>
+      </div>
+    </template>
+  </modal>
 
 </template>
 
 <script>
-    import Modal from './../layouts/Modal'
+  import Modal from './../layouts/Modal'
 
-    export default {
-        name: "LoginModal",
-        methods: {
-            closeModal() {
-                this.$emit('close')
-            }
-        },
-        components: {
-            Modal
-        }
+  export default {
+    name: "LoginModal",
+    methods: {
+      closeModal() {
+        this.$emit('close')
+      }
+    },
+    components: {
+      Modal
     }
+  }
 </script>
 
 <style scoped>
